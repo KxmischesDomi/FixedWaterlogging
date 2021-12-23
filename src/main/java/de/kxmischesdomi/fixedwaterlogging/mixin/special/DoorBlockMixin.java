61f74acb561ab8a7e584d0c55ac9e27f4e61d63e
KxmischesDomi <kxmischesdomi@gmail.com>
2021-12-23 18:40:59 +0100

    Fix waterlogging default state issues

--- src/main/java/de/kxmischesdomi/fixedwaterlogging/mixin/special/DoorBlockMixin.java
+++ src/main/java/de/kxmischesdomi/fixedwaterlogging/mixin/special/DoorBlockMixin.java
@@ -9,16 +9,12 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DoorBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 /**
  * @author KxmischesDomi | https://github.com/kxmischesdomi
@@ -29,23 +25,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(DoorBlock.class)
 public abstract class DoorBlockMixin extends Block {
 
-	@Shadow @Final public static EnumProperty<DoubleBlockHalf> HALF;
-
 	public DoorBlockMixin(Properties properties) {
 		super(properties);
 	}
 
 	/**
 	 * Makes the upper door not copy the waterlogged property of the lower door part
-	 * TODO: CHANGE TO NOT REPLACE THE {@link Level#setBlock(BlockPos, BlockState, int)} CALL
 	 */
-	@Inject(method = "setPlacedBy", at = @At("HEAD"), cancellable = true)
-	public void updateShapeWaterlogged(Level level, BlockPos blockPos, BlockState blockState, LivingEntity livingEntity, ItemStack itemStack, CallbackInfo ci) {
+	@ModifyArgs(method = "setPlacedBy", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"))
+	public void setPlacedByWaterlogged(Args args, Level level, BlockPos blockPos, BlockState blockState, LivingEntity livingEntity, ItemStack itemStack) {
 		if (FixedWaterloggingMod.supportsWaterlogged(this)) {
 			FluidState fluidState = level.getFluidState(blockPos.above());
 			boolean water = fluidState.getType() == Fluids.WATER;
-			level.setBlock(blockPos.above(), blockState.setValue(HALF, DoubleBlockHalf.UPPER).setValue(BlockStateProperties.WATERLOGGED, water), 3);
-			ci.cancel();
+			args.set(1, args.<BlockState>get(1).setValue(BlockStateProperties.WATERLOGGED, water));
 		}
 	}
 
