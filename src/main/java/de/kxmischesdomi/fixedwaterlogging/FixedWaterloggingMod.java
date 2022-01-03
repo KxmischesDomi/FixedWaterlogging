796ffe9324bc3209797ff371bedc57a3dcb6fdf0
KxmischesDomi <kxmischesdomi@gmail.com>
2022-01-03 13:01:44 +0100

    Add statement library
    Not stable at the moment

--- src/main/java/de/kxmischesdomi/fixedwaterlogging/FixedWaterloggingMod.java
+++ src/main/java/de/kxmischesdomi/fixedwaterlogging/FixedWaterloggingMod.java
@@ -2,7 +2,10 @@ package de.kxmischesdomi.fixedwaterlogging;
 
 import de.kxmischesdomi.fixedwaterlogging.common.FixedWaterlogging;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.Registry;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.properties.Property;
+import virtuoel.statement.api.StateRefresher;
 
 /**
  * @author KxmischesDomi | https://github.com/kxmischesdomi
@@ -12,7 +15,14 @@ public class FixedWaterloggingMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		
+		// Didn't work properly on its own
+//		RegistryEntryAddedCallback.event(Registry.BLOCK).register(((rawId, id, block) -> {
+//			if(block instanceof FixedWaterlogging) {
+//				injectBlockProperty(block, BlockStateProperties.WATERLOGGED, false);
+//				StateRefresher.INSTANCE.reorderBlockStates();
+//			}
+//		}));
+//		StateRefresher.INSTANCE.reorderBlockStates();
 	}
 
 	/**
@@ -21,6 +31,18 @@ public class FixedWaterloggingMod implements ModInitializer {
 	 */
 	public static boolean supportsWaterlogged(Block block) {
 		return FixedWaterlogging.class.isAssignableFrom(block.getClass());
+	}
+
+	public static <T extends Comparable<T>> void injectBlockProperty(Class<? extends Block> cls, Property<T> property, T defaultValue) {
+		for(Block block : Registry.BLOCK) {
+			if(cls.isAssignableFrom(block.getClass())) {
+				injectBlockProperty(block, property, defaultValue);
+			}
+		}
+	}
+
+	public static <T extends Comparable<T>> void injectBlockProperty(Block block, Property<T> property, T defaultValue) {
+		StateRefresher.INSTANCE.addBlockProperty(block, property, defaultValue);
 	}
 
 }
